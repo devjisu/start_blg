d9e9e2fd977e1193babe0572c9676b0135125fb3
ljs7908 <dev_jisu@naver.com>
2020-03-17 00:52:40 +0900

    20200316 commit

--- L/backend/src/api/posts/posts.ctrl.js
+++ L/backend/src/api/posts/posts.ctrl.js
@@ -1,4 +1,17 @@
 import Post from '../../models/post';
+import mongoose from 'mongoose';
+import Joi from 'joi';
+
+const { ObjectId } = mongoose.Types;
+
+export const checkObjectId = (ctx, next) => {
+  const { id } = ctx.params;
+  if (!ObjectId.isValid(id)) {
+    ctx.status = 400;
+    return;
+  }
+  return next();
+};
 /*
 POST /api/posts
 {
@@ -8,13 +21,27 @@ POST /api/posts
 }
 */
 export const write = async ctx => {
+  const schema = Joi.object().keys({
+    title: Joi.string().required(),
+    body: Joi.string().required(),
+    tags: Joi.array()
+      .items(Joi.string())
+      .required(),
+  });
+
+  const result = Joi.validate(ctx.request.body, schema);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
   const { title, body, tags } = ctx.request.body;
   const post = new Post({
     title,
     body,
     tags,
   });
-
   try {
     await post.save();
     ctx.body = post;
@@ -22,6 +49,7 @@ export const write = async ctx => {
     ctx.throw(500, e);
   }
 };
+
 /*
 GET /api/posts
 */
@@ -72,6 +100,20 @@ PATCH /api/posts/:id
 
 export const update = async ctx => {
   const { id } = ctx.params;
+
+  const schema = Joi.object().keys({
+    title: Joi.string(),
+    body: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+  });
+
+  const result = Joi.validate(ctx.request.body, schema);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
   try {
     const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
       new: true,
