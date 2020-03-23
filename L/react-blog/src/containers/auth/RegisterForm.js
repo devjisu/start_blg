86df4445cfc48b721d15f35891b725814e23d694
ljs7908 <dev_jisu@naver.com>
2020-03-24 02:14:21 +0900

    756 good boy

--- L/react-blog/src/containers/auth/RegisterForm.js
+++ L/react-blog/src/containers/auth/RegisterForm.js
@@ -1,30 +1,51 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { changeField, initializeForm } from '../../modules/auth';
-import AuthForm from '../../component/auth/AuthForm';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { changeField, initializeForm, register } from "../../modules/auth";
+import AuthForm from "../../component/auth/AuthForm";
 
 const RegisterForm = () => {
   const dispatch = useDispatch();
-  const { form } = useSelector(({ auth }) => ({ form: auth.register }));
+  const { form, auth, authError } = useSelector(({ auth }) => ({
+    form: auth.register,
+    auth: auth.auth,
+    authError: auth.authError
+  }));
 
   const onChange = e => {
     const { value, name } = e.target;
     dispatch(
       changeField({
-        form: 'register',
+        form: "register",
         key: name,
-        value,
-      }),
+        value
+      })
     );
   };
 
   const onSubmit = e => {
     e.preventDefault();
+    const { username, password, passwordConfirm } = form;
+    if (password !== passwordConfirm) {
+      return;
+    }
+    dispatch(register({ username, password }));
   };
 
   useEffect(() => {
-    dispatch(initializeForm('register'));
+    dispatch(initializeForm("register"));
   }, [dispatch]);
+
+  useEffect(() => {
+    if (authError) {
+      console.log("오류발생");
+      console.log(authError);
+      return;
+    }
+    if (auth) {
+      console.log("회원가입 성공");
+      console.log(auth);
+    }
+  }, [auth, authError]);
 
   return (
     <AuthForm
