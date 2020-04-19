507b800aea0382151f436bc8b1e91d79d81d606c
devjisu <62094644+devjisu@users.noreply.github.com>
2020-04-19 22:26:23 +0900

    ....

--- L/react-blog/src/pages/WritePage.js
+++ L/react-blog/src/pages/WritePage.js
@@ -3,11 +3,12 @@ import Editor from "../component/write/Editor";
 import TagBox from "../component/write/TagBox";
 import WriteActionButtons from "../component/write/WriteActionButtons";
 import Responsive from "../component/common/Responsive";
+import EditorContainer from "../containers/write/EditorContainer"
 
 const WritePage = () => {
   return (
     <Responsive>
-      <Editor />
+      <EditorContainer />
       <TagBox />
       <WriteActionButtons />
     </Responsive>
