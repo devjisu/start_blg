8777e0701c242172904b7c3986f9196e88ce01f0
yh88.kim <dbgus444@gmail.com>
2020-04-24 02:13:51 +0900

    완료

--- K/blog/blog-frontend/src/index.js
+++ K/blog/blog-frontend/src/index.js
@@ -10,6 +10,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer, { rootSaga } from './modules';
 import { tempSetUser, check } from './modules/user';
+import { HelmetProvider } from 'react-helmet-async';
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
@@ -25,7 +26,7 @@ function loadUser() {
     store.dispatch(tempSetUser(user));
     store.dispatch(check());
   } catch (e) {
-    console.log('localStorage is not working'); 
+    console.log('localStorage is not working');
   }
 }
 
@@ -35,7 +36,9 @@ loadUser();
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
