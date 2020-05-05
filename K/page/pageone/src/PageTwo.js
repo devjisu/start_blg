4ca1ee069e9a5dd39ef806d445ae1a25f83a38d6
yh88.kim <dbgus444@gmail.com>
2020-05-06 04:39:30 +0900

    page two 기본 틀

--- K/page/pageone/src/PageTwo.js
+++ K/page/pageone/src/PageTwo.js
@@ -1,3 +1,12 @@
 import React from 'react';
 import palette from './lib/styles/palette';
 import styled from 'styled-components';
+
+const PageTwo = () => {
+    return (
+        <>
+        </>
+    );
+};
+
+export default PageOne;
